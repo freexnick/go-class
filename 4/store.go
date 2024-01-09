f996319f147a7f4a343cd00838f893c475a32934
Nika <freexnick@yahoo.com>
2024-01-09 23:10:37 +0400

    homework_4:store

---
+++ 4/store.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type dollars float32
+type database map[string]dollars
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) add(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	if _, ok := db[item]; ok {
+		msg := fmt.Sprintf("duplicate item: %q", item)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		msg := fmt.Sprintf("invalid price: %q", price)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	db[item] = dollars(p)
+
+	fmt.Fprintf(w, "added %s with price %s\n", item, db[item])
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	if _, ok := db[item]; !ok {
+		msg := fmt.Sprintf("no such item: %q", item)
+		http.Error(w, msg, http.StatusNotFound)
+		return
+	}
+
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		msg := fmt.Sprintf("invalid price: %q", price)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	db[item] = dollars(p)
+
+	fmt.Fprintf(w, "updated %s with price %s\n", db[item], item)
+}
+
+func (db database) find(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+
+	if _, ok := db[item]; !ok {
+		msg := fmt.Sprintf("no such item: %q", item)
+		http.Error(w, msg, http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "item %s has price %s\n", item, db[item])
+}
+
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+
+	if _, ok := db[item]; !ok {
+		msg := fmt.Sprintf("no such item: %q", item)
+		http.Error(w, msg, http.StatusNotFound)
+		return
+	}
+
+	delete(db, item)
+
+	fmt.Fprintf(w, "item %s has been deleted", item)
+}
+
+func main() {
+	db := database{
+		"shoes": 50,
+		"socks": 5,
+	}
+
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/create", db.add)
+	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/find", db.find)
+	http.HandleFunc("/delete", db.delete)
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
