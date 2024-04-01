150ba3885b4d5d4bdbc4af42cc8d248df7fce6a3
Nika <freexnick@yahoo.com>
2024-04-01 18:19:59 +0400

    homework_4:store_update

--- 4/main.go
+++ 4/main.go
@@ -5,26 +5,36 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type dollars float32
-type database map[string]dollars
+type database struct {
+	mu sync.Mutex
+	db map[string]dollars
+}
 
 func (d dollars) String() string {
 	return fmt.Sprintf("$%.2f", d)
 }
 
-func (db database) list(w http.ResponseWriter, req *http.Request) {
-	for item, price := range db {
+func (d *database) list(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	for item, price := range d.db {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
 }
 
-func (db database) add(w http.ResponseWriter, req *http.Request) {
+func (d *database) add(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
 	item := req.URL.Query().Get("item")
 	price := req.URL.Query().Get("price")
 
-	if _, ok := db[item]; ok {
+	if _, ok := d.db[item]; ok {
 		msg := fmt.Sprintf("duplicate item: %q", item)
 		http.Error(w, msg, http.StatusBadRequest)
 		return
@@ -37,16 +47,19 @@ func (db database) add(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	db[item] = dollars(p)
+	d.db[item] = dollars(p)
 
-	fmt.Fprintf(w, "added %s with price %s\n", item, db[item])
+	fmt.Fprintf(w, "added %s with price %s\n", item, d.db[item])
 }
 
-func (db database) update(w http.ResponseWriter, req *http.Request) {
+func (d *database) update(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
 	item := req.URL.Query().Get("item")
 	price := req.URL.Query().Get("price")
 
-	if _, ok := db[item]; !ok {
+	if _, ok := d.db[item]; !ok {
 		msg := fmt.Sprintf("no such item: %q", item)
 		http.Error(w, msg, http.StatusNotFound)
 		return
@@ -59,41 +72,49 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	db[item] = dollars(p)
+	d.db[item] = dollars(p)
 
-	fmt.Fprintf(w, "updated %s with price %s\n", db[item], item)
+	fmt.Fprintf(w, "updated %s with price %s\n", d.db[item], item)
 }
 
-func (db database) find(w http.ResponseWriter, req *http.Request) {
+func (d *database) find(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
 	item := req.URL.Query().Get("item")
 
-	if _, ok := db[item]; !ok {
+	if _, ok := d.db[item]; !ok {
 		msg := fmt.Sprintf("no such item: %q", item)
 		http.Error(w, msg, http.StatusNotFound)
 		return
 	}
 
-	fmt.Fprintf(w, "item %s has price %s\n", item, db[item])
+	fmt.Fprintf(w, "item %s has price %s\n", item, d.db[item])
 }
 
-func (db database) delete(w http.ResponseWriter, req *http.Request) {
+func (d *database) delete(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
 	item := req.URL.Query().Get("item")
 
-	if _, ok := db[item]; !ok {
+	if _, ok := d.db[item]; !ok {
 		msg := fmt.Sprintf("no such item: %q", item)
 		http.Error(w, msg, http.StatusNotFound)
 		return
 	}
 
-	delete(db, item)
+	delete(d.db, item)
 
 	fmt.Fprintf(w, "item %s has been deleted", item)
 }
 
-func main() {
+func runServer() {
 	db := database{
-		"shoes": 50,
-		"socks": 5,
+		db: map[string]dollars{
+			"shoes": 50,
+			"socks": 5,
+		},
 	}
 
 	http.HandleFunc("/list", db.list)
@@ -103,4 +124,8 @@ func main() {
 	http.HandleFunc("/delete", db.delete)
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func main() {
+	runServer()
 }
